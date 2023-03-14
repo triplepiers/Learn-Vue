064b25100af3f95f5b8fd28c52a5dc52dd7efbac
seabee <517032401@qq.com>
2023-03-15 00:17:23 +0800

    project init

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header">
+    <div class="title">后台管理</div>
+    <div class="blank"></div>
+    <div class="list">下拉框</div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+.header {
+    height: 50px;
+    line-height: 50px;
+    border-bottom: 1px solid #ccc;
+    display: flex;
+}
+.header .title {
+    width: 200px;
+}
+.header .blank {
+    flex: 1;
+}
+.header .list {
+    width: 100px;
+}
+</style>
